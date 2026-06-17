20a5d86d901018e42e599654c613339150980f90
Akash Ganapathy <benitogag777@gmail.com>
2026-06-17 15:03:36 +0530

    Added search inputs, sort dropdowns and sidebar active state

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  emailFocused = false;
+  passwordFocused = false;
+  showPassword = false;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // TODO: replace with real auth call later
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+}
